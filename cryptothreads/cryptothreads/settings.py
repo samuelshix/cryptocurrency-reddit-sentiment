cfd117e11b76d563ba6e4cd45e6929e6980dd23f
samshi <samshi@iu.edu>
2022-06-10 17:38:28 -0400

    vercel

--- cryptothreads/cryptothreads/settings.py
+++ cryptothreads/cryptothreads/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
 DEBUG = os.getenv("DEBUG", "False") == "True"
 
 # ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
-
+ALLOWED_HOSTS = ['.vercel.app']
 
 # Application definition
 

4e31080b1486875fed8bfc496ec78f375f5af4e2
samshi <samshi@iu.edu>
2022-06-14 15:31:10 -0400

    gitignore db sqlite

--- cryptothreads/app/management/commands/reddit_api.py
+++ cryptothreads/app/management/commands/reddit_api.py
@@ -6,7 +6,7 @@ from datetime import datetime
 from app.models import Submission, Comment, Topic
 from django.core.management.base import BaseCommand, CommandError
 import requests
-
+import re
 class Command(BaseCommand):
     def __init__(self):
         super(Command, self).__init__()
@@ -18,46 +18,78 @@ class Command(BaseCommand):
         user_agent="web:crypto-comment-sentiment:v1.0.0 (by /u/kash_sam_)",
         )
 
-    def get_discussion_submissions(self):
-        return self.api.search_submissions(user = 'AutoModerator',
-                                    title = 'Daily Discussion',
-                                    subreddit = 'cryptocurrency',
-                                    sort = 'desc',
-                                    num_comments = '>50',
-                                    limit = 2000)
+    def add_arguments(self, parser):
+        parser.add_argument(
+            'subreddit',
+            nargs=1,
+            type=str
+        )
+    # def get_discussion_submissions(self):
+        # return self.api.search_submissions(user = 'AutoModerator',
+        #                             title = 'Daily Discussion',
+        #                             subreddit = 'cryptocurrency',
+        #                             sort = 'desc',
+        #                             num_comments = '>50',
+        #                             limit = 2000)
 
-    def process_df(self):
-        df = pd.DataFrame(self.get_discussion_submissions())
+    def get_discussion_submissions(self, subreddit):
+        print('Using {0} subreddit...'.format(subreddit))
+        if subreddit == 'cryptocurrency':
+            return self.api.search_submissions(user = 'AutoModerator',
+                            title = 'Daily Discussion',
+                            subreddit = 'cryptocurrency',
+                            sort = 'desc',
+                            num_comments = '>50',
+                            limit = 2000)
+        elif subreddit == 'ethtrader':
+            return self.api.search_submissions(
+                                        title = 'Daily Discussion',
+                                        subreddit = 'ethtrader',
+                                        sort = 'desc',
+                                        num_comments = '>100',
+                                        )
+
+    def process_df(self, subreddit):
+        df = pd.DataFrame(self.get_discussion_submissions(subreddit))
         # df[["created_utc"]] = df[["created_utc"]].apply(pd.to_datetime, unit='s')
         # df['created_utc'] = df['created_utc'].dt.date
         crypto_threads = df[['id','num_comments','created_utc']]
+        print(crypto_threads)
         return crypto_threads
 
     # to get top 10 for each day on multi-day post: sort by popularity, 
-    def get_comments(self):
+    def get_comments(self, subreddit):
         gen_topic = Topic.objects.filter(type='gen')[0]
         btc_topic = Topic.objects.filter(type='btc')[0]
         eth_topic = Topic.objects.filter(type='eth')[0]
-        df = self.process_df()
+        df = self.process_df(subreddit)
         data = []
         for i in df.iterrows():
+            # print(i[1])
             submission = self.reddit.submission(i[1].id)
             submission.comment_sort = "top"
             comment_dict_list = []
-            for j, top_level_comment in enumerate(submission.comments[0:100]):
+            if i[1]['num_comments'] > 2000:
+                end_index = 200
+            else:
+                end_index = 10
+            for j, top_level_comment in enumerate(submission.comments[0:end_index]):
                 topics = []
-                special_topic = False
-                if j <= 10:
-                    topics.append(gen_topic)
-                body_lower = top_level_comment.body.lower()
-                if ' eth ' in body_lower or 'ethereum' in body_lower:
-                    topics.append(eth_topic)
-                    special_topic = True
-                if ' btc ' in body_lower or 'bitcoin' in body_lower:
-                    topics.append(btc_topic)
-                    special_topic = True
-                if not special_topic and j > 10:
-                    continue
+                topics.append(gen_topic)
+                topics.append(eth_topic)
+
+                # special_topic = False
+                # if j <= 10:
+                #     topics.append(gen_topic)
+                # body_lower = top_level_comment.body.lower()
+                # if ' eth ' in body_lower or 'ethereum' in body_lower:
+                #     topics.append(eth_topic)
+                #     special_topic = True
+                # if ' btc ' in body_lower or 'bitcoin' in body_lower:
+                #     topics.append(btc_topic)
+                #     special_topic = True
+                # if not special_topic and j > 10:
+                #     continue
                 comment_dict = {
                     'id': top_level_comment.id,
                     'text': top_level_comment.body,
@@ -65,17 +97,20 @@ class Command(BaseCommand):
                     'date': datetime.utcfromtimestamp(top_level_comment.created_utc).strftime('%Y-%m-%d'),
                     'topic': topics
                     }
+                # print(comment_dict)
                 comment_dict_list.append(comment_dict)
             data.append({
                 'id': i[1].id,
                 'post_date': datetime.utcfromtimestamp(i[1].created_utc).strftime('%Y-%m-%d'),
                 'comments': comment_dict_list
             })
+        print(data)
         return data
 
-    def append_data(self):
-        for i in self.get_comments():
-            submission = Submission(date=i['post_date'], id=i['id'])
+    def append_data(self, subreddit):
+        for i in self.get_comments(subreddit):
+            print(i)
+            submission = Submission(date=i['post_date'], id=i['id'], subreddit=subreddit)
             submission.save()
             # self.stdout.write(self.style.SUCCESS('Submission saved'))
             for j in i['comments']:
@@ -88,7 +123,8 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         try:
-            self.append_data()
+            self.append_data(options['subreddit'][0])
+            print('Success!')
             # self.stdout.write(self.style.SUCCESS('Success!'))
         except:
             raise CommandError('An error has occurred.')
--- cryptothreads/app/models.py
+++ cryptothreads/app/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Submission(models.Model):
     id = models.TextField(max_length=20, primary_key=True)
     date = models.DateField()
+    subreddit = models.TextField(max_length=20, default="cryptocurrency")
 
 class Topic(models.Model):
     type = models.CharField(max_length = 3)

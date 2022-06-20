e106bef85065aaa06c034572c83417b61169ae6b
samshi <samshi@iu.edu>
2022-06-19 22:57:45 -0400

    urls

--- cryptothreads/cryptothreads/urls.py
+++ cryptothreads/cryptothreads/urls.py
@@ -15,10 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('app.urls')),
 ]
-urlpatterns += staticfiles_urlpatterns()
